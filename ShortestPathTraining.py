e78ab1a1c3ac5e6d70dd57632681d559d19355a8
dhruvaravind7 <134658127+dhruvaravind7@users.noreply.github.com>
2023-09-20 18:23:17 -0700

    update

--- ShortestPathTraining.py
+++ ShortestPathTraining.py
@@ -52,19 +52,20 @@ def imshow(img):
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
+    def forward(self, x, start, goal, currPos):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        xnew = torch.concat(x, start, goal, currPos)
+        x = F.relu(self.fc2(xnew))
         x = self.fc3(x)
         return x
 
@@ -72,7 +73,8 @@ if __name__ == '__main__':
     
     net = Net()
 
-
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     dataset = readData()
 
     for epoch in range(2):
@@ -85,23 +87,17 @@ if __name__ == '__main__':
                 start = inputs[j][1]
                 end = inputs[j][2]
                 intermediate_labels = labels[j]
+                image_t = torch.tensor(np.array(image), dtype=torch.float32)
+                start_t = torch.tensor(np.array(start), dtype=torch.float32)
+                end_t = torch.tensor(np.array(end), dtype=torch.float32)
+                #do same for start and end
                 pdb.set_trace()
-
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(2):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(inputs)
+            outputs = net(inputs, start_t, end_t)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
